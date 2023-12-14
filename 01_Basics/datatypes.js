762be49f465f58cb7d389608691a4d2d08585864
Roshan Shah <roshanshah639@gmail.com>
2023-12-14 16:33:31 +0545

    datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,20 @@
+// Primitives Types => String,Number,Boolean, Undefined, Null, Symbol, BigInt;
+
+let name = "John"; // String
+
+let age = 30; // Number
+
+let isApproved = false; // Boolean
+
+let firstName = undefined; // Undefined
+
+let lastName = null; // Null
+
+let sym = Symbol("B"); // Symbol
+// console.log(typeof sym);
+
+let big = 9007199254740991n; // BigInt
+
+
+
+// Reference Types => Objectc, Function, Array
---
+++ 01_Basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = "1234";
+let accountEmail = "2vJiC@example.com";
+var accountPassword = "password";
+accountCity = "New York";
+let accountState;
+
+
+accountId = "5678";
+
+accountEmail = "abc@example.com";
+accountPassword = "newpassword";
+accountCity = "Los Angeles";
+console.table({
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState
+});
